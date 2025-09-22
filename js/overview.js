9adec442ee6b973026792d4f7ece6adaa932b60f
HerndonE <EHerndon17@gmail.com>
2025-09-22 17:57:50 -0600

    [add] put additional graphs

--- js/overview.js
+++ js/overview.js
@@ -59,8 +59,254 @@ function factionPopularity(containerId, chartData) {
     });
 }
 
+function mapCountBarChart(
+  containerId,
+  chartData,
+  expandBtnSelector = "#expand-btn"
+) {
+  const fullData = Object.entries(chartData)
+    .map(([name, value]) => ({ name, value }))
+    .sort((a, b) => b.value - a.value);
+
+  const defaultItemCount = 10;
+  const svgHeight = 450;
+  const margin = { top: 20, right: 20, bottom: 100, left: 50 };
+
+  const svg = d3.select(containerId).append("svg").attr("height", svgHeight);
+
+  const tooltip = d3.select("#tooltip");
+  const expandBtn = document.querySelector(expandBtnSelector);
+  let isExpanded = false;
+
+  function drawChart(dataSubset) {
+    svg.selectAll("*").remove();
+
+    const maxSvgWidth = 1200;
+    const minSvgWidth = 500;
+    const computedWidth = Math.max(
+      minSvgWidth,
+      Math.min(dataSubset.length * 60, maxSvgWidth)
+    );
+    svg.attr("width", computedWidth);
+
+    const width = computedWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
+
+    const x = d3
+      .scaleBand()
+      .domain(dataSubset.map((d) => d.name))
+      .range([margin.left, width + margin.left])
+      .padding(0.2);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataSubset, (d) => d.value)])
+      .nice()
+      .range([height + margin.top, margin.top]);
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    // Add x-axis group with .axis class
+    svg
+      .append("g")
+      .attr("class", "axis") // <-- Added here
+      .attr("transform", `translate(0, ${height + margin.top})`)
+      .call(xAxis)
+      .selectAll("text")
+      .attr("transform", "rotate(90)")
+      .attr("x", 9)
+      .attr("y", 0)
+      .attr("dy", ".35em")
+      .style("text-anchor", "start");
+
+    // Add y-axis group with .axis class
+    svg
+      .append("g")
+      .attr("class", "axis") // <-- Already here, confirmed
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(yAxis);
+
+    svg
+      .selectAll(".bar")
+      .data(dataSubset)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => height + margin.top - y(d.value))
+      .attr("fill", "steelblue")
+      .on("mouseover", (event, d) => {
+        tooltip
+          .style("opacity", 1)
+          .html(`<strong>${d.name}</strong><br>Count: ${d.value}`);
+      })
+      .on("mousemove", (event) => {
+        tooltip
+          .style("left", event.pageX + 10 + "px")
+          .style("top", event.pageY - 28 + "px");
+      })
+      .on("mouseout", () => {
+        tooltip.style("opacity", 0);
+      });
+
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -(height / 2) - margin.top)
+      .attr("y", margin.left - 35)
+      .attr("text-anchor", "middle")
+      .text("Count");
+  }
+
+  drawChart(fullData.slice(0, defaultItemCount));
+
+  if (expandBtn && fullData.length > defaultItemCount) {
+    expandBtn.style.display = "inline-block";
+    expandBtn.textContent = "Expand Chart";
+
+    expandBtn.addEventListener("click", () => {
+      isExpanded = !isExpanded;
+      const dataToUse = isExpanded
+        ? fullData
+        : fullData.slice(0, defaultItemCount);
+      drawChart(dataToUse);
+      expandBtn.textContent = isExpanded ? "Collapse Chart" : "Expand Chart";
+    });
+  }
+}
+
+function commanderCountBarChart(
+  containerId,
+  chartData,
+  expandBtnSelector = "#expand-btn"
+) {
+  const fullData = chartData
+    .map(([name, value]) => ({ name, value }))
+    .sort((a, b) => b.value - a.value);
+
+  const defaultItemCount = 10;
+  const svgHeight = 450;
+  const margin = { top: 20, right: 20, bottom: 100, left: 50 };
+
+  const svg = d3.select(containerId).append("svg").attr("height", svgHeight);
+
+  const tooltip = d3.select("#tooltip");
+  const expandBtn = document.querySelector(expandBtnSelector);
+  let isExpanded = false;
+
+  function drawChart(dataSubset) {
+    svg.selectAll("*").remove();
+
+    const maxSvgWidth = 1200;
+    const minSvgWidth = 500;
+    const computedWidth = Math.max(
+      minSvgWidth,
+      Math.min(dataSubset.length * 60, maxSvgWidth)
+    );
+    svg.attr("width", computedWidth);
+
+    const width = computedWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
+
+    const x = d3
+      .scaleBand()
+      .domain(dataSubset.map((d) => d.name))
+      .range([margin.left, width + margin.left])
+      .padding(0.2);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataSubset, (d) => d.value)])
+      .nice()
+      .range([height + margin.top, margin.top]);
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    svg
+      .append("g")
+      .attr("class", "axis")
+      .attr("transform", `translate(0, ${height + margin.top})`)
+      .call(xAxis)
+      .selectAll("text")
+      .attr("transform", "rotate(90)")
+      .attr("x", 9)
+      .attr("y", 0)
+      .attr("dy", ".35em")
+      .style("text-anchor", "start");
+
+    svg
+      .append("g")
+      .attr("class", "axis")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(yAxis);
+
+    svg
+      .selectAll(".bar")
+      .data(dataSubset)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => height + margin.top - y(d.value))
+      .attr("fill", "steelblue")
+      .on("mouseover", (event, d) => {
+        tooltip
+          .style("opacity", 1)
+          .html(`<strong>${d.name}</strong><br>Count: ${d.value}`);
+      })
+      .on("mousemove", (event) => {
+        tooltip
+          .style("left", event.pageX + 10 + "px")
+          .style("top", event.pageY - 28 + "px");
+      })
+      .on("mouseout", () => {
+        tooltip.style("opacity", 0);
+      });
+
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -(height / 2) - margin.top)
+      .attr("y", margin.left - 35)
+      .attr("text-anchor", "middle")
+      .text("Count");
+  }
+
+  drawChart(fullData.slice(0, defaultItemCount));
+
+  if (expandBtn && fullData.length > defaultItemCount) {
+    expandBtn.style.display = "inline-block";
+    expandBtn.textContent = "Expand Chart";
+
+    expandBtn.addEventListener("click", () => {
+      isExpanded = !isExpanded;
+      const dataToUse = isExpanded
+        ? fullData
+        : fullData.slice(0, defaultItemCount);
+      drawChart(dataToUse);
+      expandBtn.textContent = isExpanded ? "Collapse Chart" : "Expand Chart";
+    });
+  }
+}
+
 d3.json("../data/mydata.json").then((rawData) => {
   const data = rawData.processed_data;
 
   factionPopularity("#inline-bar-chart-1", data.processed_most_played_factions);
+  mapCountBarChart(
+    "#chart-wrapper-played-maps-1",
+    data.processed_map_counts,
+    "#expand-btn-played-maps-1"
+  );
+  commanderCountBarChart(
+    "#chart-wrapper-active-commanders-1",
+    data.processed_commander_list,
+    "#expand-btn-active-commanders-1"
+  );
 });
